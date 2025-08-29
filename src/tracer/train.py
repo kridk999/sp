d808f9d65f2799d7261a1d10179d5fddd2808a65
kridk999 <s214609@student.dtu.dk>
2025-08-29 16:00:16 +0200

    cookiecutter init commit

---
+++ src/tracer/train.py
@@ -0,0 +1,10 @@
+from tracer.model import Model
+from tracer.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
